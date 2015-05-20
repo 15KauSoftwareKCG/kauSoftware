ea231bbefe51e38ce6aec03ff6195c31072c4cc8
ojh3004 <ojh3004@gmail.com>
2015-05-20 17:04:31 +0900

    결합0520
    마커 아이콘, 검색 리스트, http 통신, JSON 처리

--- project_workspace/tMapTest/src/com/example/tmaptest/MainActivity.java
+++ project_workspace/tMapTest/src/com/example/tmaptest/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.tmaptest;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.LogManager;
 
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xml.sax.SAXException;
 
 import com.skp.Tmap.TMapAddressInfo;
@@ -39,6 +54,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Criteria;
@@ -102,12 +118,18 @@ public class MainActivity extends Activity {
 	double latSoowon = 37.265669;
 	double lonSoowon = 127.000223;
 		
+	// 서울역
+	double latSeoul = 37.554705;
+	double lonSeoul = 126.970688223;
+	
 	String textEndPoint = "";
 	
 	TMapPOIItem item = new TMapPOIItem();
 	ArrayList<TMapPOIItem> POIItem = new ArrayList<TMapPOIItem>();
 	int zoom = 15; // defualt zoom
 	  
+	TMapAddressInfo addressInfoSave = new TMapAddressInfo();
+	
 	TMapView mMapView;;
 	LocationManager mLocMgr;
 	String locProv;
@@ -258,7 +280,18 @@ public class MainActivity extends Activity {
 	    				}
     				}
 	    			if(check==1) popMarker.setTitle("상세 정보");
-	    			else popMarker.setTitle("사용자 지정");
+	    			else
+	    			{
+	    				// 사용자 지정 마커 롱클릭
+	    				popMarker.setTitle("사용자 지정");
+	    				pName.setText("");
+						pPhone.setText("");
+						//pAddress.setText("");
+						
+						pAddress.setText("　주소: "+ addressInfoSave.strFullAddress);
+						
+						Log.d(TAG, "사용자 마커 클릭");
+					}
 	    			endPoint.setLatitude(temp.latitude);
 					endPoint.setLongitude(temp.longitude);
 					
@@ -275,14 +308,28 @@ public class MainActivity extends Activity {
 			    	tMapMarkerItem.setName("사용자 지정");
 			    	tMapMarkerItem.setVisible(tMapMarkerItem.VISIBLE);
 			    	tMapMarkerItem.setTMapPoint(point);
-			    	Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pin_b_b_o);
+			    	Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pin_b_bm_o);
 			    	tMapMarkerItem.setIcon(bitmap);
+			    	tMapMarkerItem.setPosition((float)0.5, (float)1.0);
+			    	
+			    	tmapdata.reverseGeocoding(point.getLatitude(), point.getLongitude(), "A03",
+			    			new reverseGeocodingListenerCallback() {
+			    			@Override
+			    			public void onReverseGeocoding(TMapAddressInfo addressInfo) {
+			    				//LogManager.printLog("선택한 위치의 주소는 " + addressInfo.strFullAddress);
+			    				//pAddress.setText(""+ addressInfo.strFullAddress);
+			    				addressInfoSave = addressInfo;
+			    			}
+			    	});
 			    	
 			    	mMapView.addMarkerItem(tMapMarkerItem.getID(), tMapMarkerItem);
 			    	pName.setText("");
 					pPhone.setText("");
-					pAddress.setText("");
+					//pAddress.setText("");
+					pAddress.setText("　주소: "+ addressInfoSave.strFullAddress);
 			    	popMarker.setTitle("사용자 지정");
+			    	
+			    	Log.d(TAG, "사용자 마커 생성");
 			    	
 			    	endPoint.setLatitude(tMapMarkerItem.latitude);
 					endPoint.setLongitude(tMapMarkerItem.longitude);
@@ -312,6 +359,8 @@ public class MainActivity extends Activity {
 				);
 		    	//mMapView.setCenterPoint((startPoint.getLongitude() + endPoint.getLongitude())/2
 		    	//		,(startPoint.getLatitude() + endPoint.getLatitude())/2);
+		    	
+		    	// startPoint, endPoint에 맞는 줌, 중심 구하기
 		    	ArrayList<TMapPoint> point = new ArrayList<TMapPoint>();
 		    	point.add(startPoint);
 		    	point.add(endPoint);
@@ -411,13 +460,16 @@ public class MainActivity extends Activity {
 
 		    public void onClick(View v) {
 
-		    	textEndPoint = et.getText().toString(); // 가상휴대폰에서 한글입력 지원안됨, 추후 확인
+		    	//textEndPoint = et.getText().toString(); // 가상휴대폰에서 한글입력 지원안됨, 추후 확인
+		    	textEndPoint="서울역";
+		    	
 		    	listadapter.clear();
 		    	tmapdata.findTitlePOI(textEndPoint, new FindTitlePOIListenerCallback() { 
 		    		
 		    		@Override
 		    		public void onFindTitlePOI(ArrayList<TMapPOIItem> poiItem) {
 
+		    		// 이전 마커 지우기
 		    		if(POIItem.isEmpty()==false)
     				{
     					for(int i=0; i<POIItem.size(); i++)
@@ -451,6 +503,8 @@ public class MainActivity extends Activity {
 		    		else tMapMarkerItem.setCalloutSubTitle("☎: "+item.telNo);
 		    		tMapMarkerItem.setVisible(tMapMarkerItem.VISIBLE);
 		    		tMapMarkerItem.setCanShowCallout(true);
+		    		Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.pin_r_bm_o);
+			    	tMapMarkerItem.setIcon(bitmap);
 		    		tMapMarkerItem.setPosition((float)0.5, (float)1.0);
 		    		
 		    		listadapter.add(item);
@@ -505,6 +559,7 @@ public class MainActivity extends Activity {
 		findViewById(R.id.button4).setOnClickListener(		
 				new Button.OnClickListener() {
 		    			public void onClick(View v) {
+		    				/*
 		    				mMapView.setIconVisibility(false);
 		    				mMapView.setZoomLevel(12);
 		    				tmapdata.findPathDataWithType(TMapPathType.BICYCLE_PATH, startPoint, endPoint,
@@ -520,6 +575,159 @@ public class MainActivity extends Activity {
 		    				//		(startPoint.getLatitude() + endPoint.getLongitude()/2));
 		    				mMapView.setCenterPoint((lonCityhall + lonKau)/2, (latCityhall + latKau)/2);
 		    				//mMapView.setCenterPoint((lonCityhall + lonSoowon)/2, (latCityhall + latSoowon)/2);
+		    				*/
+		    				
+		    				Thread thread = new Thread() {
+		                        @Override
+		                        public void run() {
+		                            HttpClient httpClient = new DefaultHttpClient();
+
+
+		                            //String urlString = "https://apis.skplanetx.com/tmap/routes/bicycle?callback=&bizAppId=&version=1";
+		                            
+		                            String urlString = "https://apis.skplanetx.com/tmap/routes/bicycle?callback=&bizAppId=&version=1&format=json&appKey=e2a7df79-5bc7-3f7f-8bca-2d335a0526e7";
+		                            //String urlString = "https://apis.skplanetx.com/tmap/routes/pedestrian?callback=&bizAppId=&version=1&format=json&appKey=e2a7df79-5bc7-3f7f-8bca-2d335a0526e7";
+		                            
+		                            TMapPolyLine jsonPolyline = new TMapPolyLine();
+							        jsonPolyline.setLineColor(Color.RED);
+							        jsonPolyline.setLineWidth(2);
+							        		                            
+		                            // &format={xml 또는 json}
+		                            try {
+		                                URI url = new URI(urlString);
+
+		                                HttpPost httpPost = new HttpPost();
+		                                httpPost.setURI(url);
+
+		                                List<BasicNameValuePair> nameValuePairs = new ArrayList<BasicNameValuePair>();
+		                                //nameValuePairs.add(new BasicNameValuePair("startX", Double.toString(126.9786599)));
+		                                //nameValuePairs.add(new BasicNameValuePair("startY", Double.toString(37.5657321)));
+		                                nameValuePairs.add(new BasicNameValuePair("startX", Double.toString(startPoint.getLongitude())));
+		                                nameValuePairs.add(new BasicNameValuePair("startY", Double.toString(startPoint.getLatitude())));
+		                                
+		                                
+		                                //nameValuePairs.add(new BasicNameValuePair("endX", Double.toString(126.9516781)));
+		                                //nameValuePairs.add(new BasicNameValuePair("endY", Double.toString(37.5426981)));
+		                                nameValuePairs.add(new BasicNameValuePair("endX", Double.toString(endPoint.getLongitude())));
+		                                nameValuePairs.add(new BasicNameValuePair("endY", Double.toString(endPoint.getLatitude())));
+		                                
+		                                
+		                                
+		                                nameValuePairs.add(new BasicNameValuePair("reqCoordType", "WGS84GEO"));
+		                                nameValuePairs.add(new BasicNameValuePair("resCoordType", "WGS84GEO"));
+		                                
+		                                nameValuePairs.add(new BasicNameValuePair("startName", "서울시청"));
+		                                nameValuePairs.add(new BasicNameValuePair("endName", "공덕역"));
+		                                
+		                                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+
+		                                HttpResponse response = httpClient.execute(httpPost);
+		                                String responseString = EntityUtils.toString(response.getEntity(), HTTP.UTF_8);
+
+		                                //Log.d(TAG, responseString);
+
+		                                String strData = "";
+		                             	
+		                                Log.d(TAG, "0\n");
+								        JSONObject jAr = new JSONObject(responseString);
+								        
+		                                Log.d(TAG, "1\n");
+										// 개별 객체를 하나씩 추출
+		                                JSONArray features = jAr.getJSONArray("features");
+								        
+								        Log.d(TAG, "2,"+ features.length() +"\n");
+								        // 객체에서 하위 객체를 추출
+								        
+								    	
+								        for(int i=0; i<features.length(); i++)
+								        {
+								        	 JSONObject test2 = features.getJSONObject(i);
+								        	 JSONObject properties = test2.getJSONObject("properties");
+
+								        	 
+								        	 JSONObject geometry = test2.getJSONObject("geometry");
+								        	 JSONArray coordinates = geometry.getJSONArray("coordinates");
+								        	
+								        	 String geoType = geometry.getString("type");
+								        	 if(geoType.equals("Point"))
+								        	 {
+								        		 double lonJson = coordinates.getDouble(0);
+									        	 double latJson = coordinates.getDouble(1);
+									        	 
+									        	 Log.d(TAG, "-");
+									        	 Log.d(TAG, lonJson+","+latJson+"\n"); 
+									        	 TMapPoint jsonPoint = new TMapPoint(latJson, lonJson);
+									        	
+									        	 jsonPolyline.addLinePoint(jsonPoint);
+									        	 Log.d(TAG, jsonPoint.getLatitude()+"-"+jsonPoint.getLongitude());
+									        	 
+									        	 
+								        	 }
+								        	 
+								        	 								        			 
+								        	 String nodeType = properties.getString("nodeType");
+								        	 
+								        	 if(nodeType.equals("POINT")){
+										        String turnType = properties.getString("turnType");
+										        	
+										        Log.d(TAG, "회전방향: "+turnType+"\n");
+										     }
+								        	 else if(nodeType.equals("LINE")){
+								        		 String description = properties.getString("description");
+								        		 Log.d(TAG, description+"\n");
+								        	 }
+								        	 
+								        }
+								        DashPathEffect dashPath = new DashPathEffect(new float[]{20,10}, 1); //점선
+								        //DashPathEffect dashPath2 = new DashPathEffect(new float[]{0,0}, 0); //실선
+								        
+								        jsonPolyline.setPathEffect(dashPath); //점선
+								        //jsonPolyline.setPathEffect(dashPath2); //실선
+								        jsonPolyline.setLineColor(Color.GREEN);
+								        jsonPolyline.setLineAlpha(0);
+								        mMapView.addTMapPolyLine("jsonPolyline",jsonPolyline);
+								        /*
+								        JSONObject test = features.getJSONObject(0);
+								        
+								        JSONObject properties = test.getJSONObject("properties");
+								        
+								        Log.d(TAG, "3\n");
+								        //JSONObject index = properties.getJSONObject("index");
+								        String nodeType = properties.getString("nodeType");
+								        
+								        
+								        Log.d(TAG, "4 " + nodeType+"\n");
+								        
+								        if(nodeType.equals("POINT")){
+								        	String turnType = properties.getString("turnType");
+								        	
+								        	Log.d(TAG, "5 " + turnType+"\n");
+								        }
+								        */
+								        
+								        // 하위 객체에서 데이터를 추출
+								        //strData += features.getString("name") + "\n";
+										
+
+		                            } catch (URISyntaxException e) {
+		                                Log.e(TAG, e.getLocalizedMessage());
+		                                e.printStackTrace();
+		                            } catch (ClientProtocolException e) {
+		                                Log.e(TAG, e.getLocalizedMessage());
+		                                e.printStackTrace();
+		                            } catch (IOException e) {
+		                                Log.e(TAG, e.getLocalizedMessage());
+		                                e.printStackTrace();
+		                            } catch (JSONException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+
+		                        }
+		                    };
+
+		                    thread.start();
 		    			}
 		    		  
 				}
